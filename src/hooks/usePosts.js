998eae8190bc2e81b8d3a3dba95323a258ea5d88
alvizeta <a.zorzini@gmail.com>
2020-05-15 02:48:35 -0300

    fix: refactor of usePosts

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,36 +1,49 @@
-import { useEffect, useState } from 'react'
+import {useCallback, useEffect, useState} from 'react'
 import axios from 'axios'
+import {useDispatch, useSelector} from "react-redux";
+import { addPosts } from "../redux/allPostsSlice";
+import usePrevious from "./usePrevious";
 
 const usePosts = (after) => {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false)
-  const [posts, setPosts] = useState([])
-
-  const searchParams = new URLSearchParams({
-    limit: '50'
-  });
-  if (after) {
-    searchParams.set('after', after);
+  const previousAfterTag = usePrevious(after);
+  const posts = useSelector(state => state.allPosts);
+  const selectedPost = useSelector(state => state.allPosts.find(p => {
+      return p.data.id === state.selectedPost;
   }
 
-  useEffect(() => {
-    setLoading(true)
-    setError(false)
-    axios({
-      method: 'GET',
-      url: `https://www.reddit.com/top.json?${searchParams.toString()}`,
-    }).then(res => {
-      setPosts(prevPosts => {
-        return ([...prevPosts, ...res.data.data.children])
+  ))
+  const dispatch = useDispatch();
+
+  const loadPosts = useCallback(async() => {
+    const searchParams = new URLSearchParams({
+      limit: '50'
+    });
+    if (after) {
+      searchParams.set('after', after);
+    }
+    try {
+      setLoading(true)
+      setError(false)
+      const response = await axios({
+        method: 'GET',
+        url: `https://www.reddit.com/top.json?${searchParams.toString()}`,
       })
+      dispatch(addPosts(response.data.data.children));
       setLoading(false)
-    }).catch(e => {
+    } catch (e) {
       if (axios.isCancel(e)) return
       setError(true)
-    })
-  }, [after])
+    }
+  }, [after]);
 
-  return { loading, error, posts }
+  useEffect(() => {
+    if(after === previousAfterTag) return ;
+    loadPosts();
+  }, [dispatch, loadPosts, previousAfterTag])
+
+  return { loading, error, posts, selectedPost, loadPosts }
 }
 
 export default usePosts;
