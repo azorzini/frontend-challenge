8631313c5df0dcb3cea3b94588de126c8ab02bd7
alvizeta <a.zorzini@gmail.com>
2020-05-15 13:25:38 -0300

    Updates

---
+++ precache-manifest.97e48ad8324dd50bfef9070a0a4dfd4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d57fe47475b0f15fb21c4f7bb4943b",
+    "url": "/frontend-challenge/index.html"
+  },
+  {
+    "revision": "ae13398bbf289921b9e1",
+    "url": "/frontend-challenge/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ab7d25a117b24d5b2fa3",
+    "url": "/frontend-challenge/static/js/2.13e083d3.chunk.js"
+  },
+  {
+    "revision": "35a08d2e06945b779fadacb826071be3",
+    "url": "/frontend-challenge/static/js/2.13e083d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae13398bbf289921b9e1",
+    "url": "/frontend-challenge/static/js/main.9dc31a48.chunk.js"
+  },
+  {
+    "revision": "c5608110c8e2cefbb862",
+    "url": "/frontend-challenge/static/js/runtime-main.a4d5dbf1.js"
+  },
+  {
+    "revision": "9006f32fcbc5385c844d7eca4a4e8fd4",
+    "url": "/frontend-challenge/static/media/reddit.9006f32f.png"
+  }
+]);
